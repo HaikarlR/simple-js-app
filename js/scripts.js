04f19d93b132eb65788a502b4b4fa6cc00213156
HaikarlR <122443850+HaikarlR@users.noreply.github.com>
2023-02-13 20:37:39 +1100

    Added notes

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ let pokemonList = [
     { name: ' Squirtle', height: 5, types: ['water']}
 ];
 
+// Using a for loop to write names and heights and adds a conditional
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 6){ 
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')   Wow, that is tall!' + '<br>');
