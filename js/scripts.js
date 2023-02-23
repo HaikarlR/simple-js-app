91ce91326ee904b8fbf519995d4a360ca321220f
HaikarlR <122443850+HaikarlR@users.noreply.github.com>
2023-02-23 21:21:59 +1100

    Updated code
    - Updated forEach function
    - Wrapped pokemonList with IIFE
    - created pokemonRepository variable
    -created getAll and add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
     { name: ' Charmander', height: 6, types: ['fire']},
     { name: ' Squirtle', height: 5, types: ['water']}
 ];
 
+function getAll(){
+    return pokemonList;
+}
+
+function add(pokemon){
+    pokemonList.push(pokemon);
+}
+
+return {
+    add: add,
+    getAll: getAll
+};
+})();
+
+pokemonRepository.add({name: 'Pikachu', height: 4, types :['electric'] });
+console.log(pokemonRepository.getAll());
 // Using a for loop to write names and heights and adds a conditional
 /*for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 6){ 
@@ -15,7 +32,7 @@ let pokemonList = [
 }*/
 
 // Replacing the for loop with a forEach loop
-pokemonList.forEach(function (pokemon){
+pokemonRepository.getAll().forEach(function (pokemon){
     if (pokemon.height > 6){
         document.write(pokemon.name + ' (height: ' + pokemon.height + ') Wow, that is tall!' + '<br>');
     } else {
